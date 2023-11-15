45a41c2509772e0e09b9e60eaec4de20d9f5f98a
Ольга Ефремова <oleffr15@yandex.ru>
2023-11-15 20:24:53 +0300

    v15

--- backend/foodgram/api/management/commands/import_data.py
+++ backend/foodgram/api/management/commands/import_data.py
@@ -7,7 +7,7 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         with open(
-            'data/ingredients.csv', encoding='utf-8'
+            'ingredients.csv', encoding='utf-8'
         ) as file:
             reader = csv.reader(file)
             next(reader)
