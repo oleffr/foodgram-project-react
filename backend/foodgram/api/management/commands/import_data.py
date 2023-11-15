71af8d4bcfcd058dd423f9d1fe12075ea5ffd292
Ольга Ефремова <oleffr15@yandex.ru>
2023-11-15 21:44:59 +0300

    v15

--- backend/foodgram/api/management/commands/import_data.py
+++ backend/foodgram/api/management/commands/import_data.py
@@ -1,23 +1,30 @@
-import csv
 import os
+import json
+import logging
 
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s, %(levelname)s, %(message)s',
+)
+
+logger = logging.getLogger(__name__)
+
+
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        with open(
-            os.path.join(os.path.dirname(__file__), 'ingredients.csv'), 'r',
-            encoding='utf-8'
-        ) as file:
-            reader = csv.reader(file)
-            next(reader)
-            ingredients = [
-                Ingredient(
-                    name=row[0],
-                    measurement_unit=row[1],
-                )
-                for row in reader
-            ]
-            Ingredient.objects.bulk_create(ingredients)
+        logger.info('Импорт начался')
+        try:
+            with open(
+                    os.path.join(os.path.dirname(__file__), 'ingredients.csv'),
+                    'r',
+                    encoding='utf-8') as file:
+                data = json.load(file)
+                Ingredient.objects.bulk_create(Ingredient(**line
+                                                          ) for line in data)
+            logger.info(f'Данные загружены')
+        except Exception as error:
+            logger.error(error)
