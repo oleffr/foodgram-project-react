2c2fec70ddf78ca105903c14c3a75b769f55baec
Ольга Ефремова <oleffr15@yandex.ru>
2023-11-16 23:08:34 +0300

    v20

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(Path(__file__).resolve().parent, 'static')
+STATIC_ROOT = os.path.join(Path(__file__).resolve().parent.parent.parent, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
