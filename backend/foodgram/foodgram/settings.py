916b113a29db93000b11103dd035e5ffa0ae2258
Ольга Ефремова <oleffr15@yandex.ru>
2023-11-16 23:34:55 +0300

    v21

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -107,8 +107,7 @@ TIME_ZONE = 'UTC'
 USE_TZ = True
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(Path(__file__).resolve().parent.parent.parent, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
