cc1ef286c5a3f53d57dd67f0dbe7b2938a44efe7
Ольга Ефремова <oleffr15@yandex.ru>
2023-11-17 16:06:08 +0300

    v23

---
+++ backend/backend/settings.py
@@ -0,0 +1,149 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FIELD_NUM_1 = 200
+FIELD_NUM_2 = 150
+AUTH_USER_MODEL = 'users.CustomUser'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+#SECRET_KEY = 'django-insecure-tsk7*6m@5u&a-b$b0g5gchxaatx&b0dtdvs3+i_^qwvmpti1h8'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# DEBUG = True
+
+# ALLOWED_HOSTS = []
+DEBUG = os.getenv("DEBUG") == 'True'
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(', ')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
+    'api.apps.ApiConfig',
+    'django_filters',
+    'colorfield',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CSV_FILE_DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.LimitPageNumberPagination',
+    'PAGE_SIZE': 6,
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'HIDE_USERS': False,
+    'SERIALIZERS': {
+        'user_create': 'api.serializers.CreateCustomUserSerializer',
+        'user': 'api.serializers.CustomUserSerializer',
+        'current_user': 'api.serializers.CustomUserSerializer',
+    },
+    'PERMISSIONS': {
+        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
+        'user_list': ['rest_framework.permissions.AllowAny'],
+    },
+}
---
+++ backend/users/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from backend.settings import FIELD_NUM_1, FIELD_NUM_2
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=FIELD_NUM_1, blank=False,
+                              verbose_name='email',
+                              unique=True)
+    first_name = models.CharField(max_length=FIELD_NUM_2, blank=False,
+                                  verbose_name='Имя')
+    last_name = models.CharField(max_length=FIELD_NUM_2, blank=False,
+                                 verbose_name='Фамилия'
+                                 )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                                   related_name='subscriber',
+                                   verbose_name='Подписчик')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                               related_name='author',
+                               verbose_name='Автор')
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = (
+            models.UniqueConstraint(
+                fields=['author', 'subscriber'],
+                name='unique_subscription'),
+            models.CheckConstraint(
+                check=~models.Q(subscriber=models.F('author')),
+                name='self_subscribe',
+            ))
