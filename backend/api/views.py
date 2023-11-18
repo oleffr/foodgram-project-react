f04788cc5f552ad0b042d0372a2331a8160f3807
Ольга Ефремова <oleffr15@yandex.ru>
2023-11-18 23:42:37 +0300

    v29

--- backend/api/views.py
+++ backend/api/views.py
@@ -163,9 +163,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     @action(
         detail=True,
-        methods=['post', 'delete'],
-        url_path='favourite',
-        url_name='favourite',
+        methods=['post', 'delete']
     )
     def get_favorite(self, request, pk):
         if request.method == 'POST':
