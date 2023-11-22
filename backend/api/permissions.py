a8ca69177d79dc9ab3c40b289313cec34b31d181
Ольга Ефремова <oleffr15@yandex.ru>
2023-11-22 22:19:16 +0300

    v39

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,9 +3,5 @@ from rest_framework import permissions
 
 class AuthorOrAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_staff
-            or request.user == obj.author
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_staff or obj.author == request.user)
