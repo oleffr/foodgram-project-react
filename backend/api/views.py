d5dab4c4673d5e5357b86adebbbd1eec2c5be186
Ольга Ефремова <oleffr15@yandex.ru>
2023-12-01 20:55:49 +0300

    views

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,9 +17,11 @@ from api.serializers import (CreateRecipeSerializer, FavoriteSerializer,
                              SubscriptionSerializer, TagSerializer,
                              UserSerializer)
 from api.utils import download_csv
-from recipes.models import (Ingredient,
+from recipes.models import (Favorite,
+                            Ingredient,
                             Recipe,
                             RecipeIngredient,
+                            ShoppingCart,
                             Tag)
 from users.models import Subscription, User
 
@@ -101,13 +103,32 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly, AuthorOrReadOnly
     )
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return (Recipe.objects.all().select_related('author')
+                    .annotate(is_favorited=Exists(
+                        Favorite.objects.filter(recipe=OuterRef('pk'))
+                        .select_related('user')
+                    ))
+                    .annotate(is_in_shopping_cart=Exists(ShoppingCart.objects
+                                                         .filter(
+                                                             recipe=OuterRef(
+                                                                 'pk'))
+                                                         .select_related(
+                                                             'user')
+                                                         ))
+                    .prefetch_related('tags', 'ingredients')
+                    )
+        return (Recipe.objects.all().select_related('author')
+                .prefetch_related('tags', 'ingredients'))
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
